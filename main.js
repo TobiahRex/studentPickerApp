efc5c825e98d36d50219f97591c009994654f488
RexCorpDev <rexcorpfx@gmail.com>
2016-06-07 20:28:39 -0700

    mvp and extras complete - css in progress

--- main.js
+++ main.js
@@ -13,21 +13,55 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
 function submitNames(){
   let names = document.getElementsByTagName('textarea')[0].value;
-  console.log('names: ', names.split(','));
-
 
   names.split(',').forEach(name => {
-    let bold = document.createElement('ol');
-    let br    = document.createElement('br');
-    bold.innerHTML = name;
-    document.querySelector('div.list-group').appendChild(bold).appendChild(br);
+    let listName = document.createElement('li');
+    listName.innerHTML = name;
+    document.querySelector('div.list-group.saved-names ol').appendChild(listName);
   })
 }
 
 function getName(){
-  console.log('get name');
+  let names = document.getElementsByTagName('textarea')[0].value.split(',');
+  let randomName = names[Math.floor(Math.random()*(names.length))];
+  document.querySelector('div.list-group.random b').innerHTML = randomName;
 }
 
 function generateTeams(){
-  console.log('generate teams');
+  let groups = document.querySelector('input.teams').value;
+  let names = document.getElementsByTagName('textarea')[0].value.split(',');
+  let teamNum = names.length / groups;
+  if(groups > names.length ) return  alert('Your input is too high. Choose fewer teams.');
+
+  if(teamNum % 1 === 0){
+    // teamNum = 3
+    let namesList = names.length;
+    // let namesPerTeam = teamNum;    // 3 teams = 2 names or 6 names total
+    let teamTotal = namesList / teamNum;
+
+    while(namesList > 0){
+      console.log('START: ', namesList);
+      while(teamTotal > 0){ // team tag
+        let teamTag = document.createElement('li');
+        teamTag.setAttribute('type', '1');
+        let namesPerTeam = teamNum;
+
+        while(namesPerTeam > 0){ // each name on team
+          let name = document.createElement('p');
+          console.log('name counter: ',namesPerTeam-1);
+          name.innerHTML = names[namesList-1];
+          teamTag.appendChild(name);
+          namesPerTeam--;
+          namesList--;
+        } // end inner loop;
+        console.log('teamTag: ', teamTag);
+        document.querySelector('div.list-group.teams ol').appendChild(teamTag);
+        teamTotal--;
+      } // end outter loop
+    }// end of outter-outter loop
+  } else {
+    console.log('decimal');
+  }
+
+  // document.querySelector('div.list-group.teams ul').innerHTML = teams;
 }
